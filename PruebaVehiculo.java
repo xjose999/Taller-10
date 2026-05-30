1ebee705ee588c5ee87d15a93e77d16944c17059
xjose999 <josejoestar80@gmail.com>
2026-05-30 01:26:24 -0500

    Ejercicio 2.3

---
+++ PruebaVehiculo.java
@@ -0,0 +1,9 @@
+public class PruebaVehiculo {
+    public static void main(String[] args) {
+        Vehiculo v = new Vehiculo();
+        Bicicleta b = new Bicicleta();
+
+        v.moverse();
+        b.moverse();
+    }
+}
